f95c97f16e4e00299a0c29fa6d9c7c68f37fd3ce
Jhoan Rios <jhoanriosmorales@gmail.com>
2026-03-09 17:40:44 -0500

    Se hacen las clases iniciales de la actividad

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -1,3 +1,28 @@
 from datetime import datetime
 
-# TODO: Add code here
+class Nota:
+    def __init__(self, text: str, page: int, date: datetime):
+        self.text: str = text
+        self.date: datetime = date
+        self.page: int = page
+
+    def __str__(self):
+        return f'{self.text} {self.date} {self.page}'
+
+class Book:
+    EXCELLENT = 3
+    GOOD = 2
+    BAD = 1
+    UNRATED = -1
+    def __init__(self, text: str, pages: int, isbn: str, title: str, author: str):
+        self.text: str = text
+        self.pages: int = pages
+        self.isbn: str = isbn
+        self.title: str = title
+        self.author: str = author
+        self.rating : int = Book.UNRATED
+        self.notes : list[Nota] = []
+
+
+
+
